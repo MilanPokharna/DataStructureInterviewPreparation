e6c0c33e13105442b6f559e9ae095b0c081e7be6
MiLaN <milan.pokharna1998@gmail.com>
2018-06-13 23:54:20 +0530

    new commit3

--- app/src/main/java/com/android/datastructureinterviewpreparation/activities/ques5.java
+++ app/src/main/java/com/android/datastructureinterviewpreparation/activities/ques5.java
@@ -1,4 +1,33 @@
 package com.android.datastructureinterviewpreparation.activities;
 
-public class ques5 {
+import android.content.res.AssetManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.android.datastructureinterviewpreparation.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class ques5 extends AppCompatActivity {
+    @BindView(R.id.ques)
+    TextView ques;
+    @BindView(R.id.question)
+    TextView question;
+    @BindView(R.id.ans)
+    TextView ans;
+    @BindView(R.id.answer)
+    TextView answer;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ques1);
+        ButterKnife.bind(this);
+        AssetManager am = getApplicationContext().getAssets();
+
+
+    }
 }
